e2f67f314587af7eb3207002a090cd4c240f26c8
amardeep <mauryaamardeep5264@gmail.com>
2026-04-20 22:22:21 +0530

    day04

---
+++ day04.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class day04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+
+        int smallest;
+
+        if (a <= b && a <= c) {
+            smallest = a;
+        } else if (b <= a && b <= c) {
+            smallest = b;
+        } else {
+            smallest = c;
+        }
+
+        System.out.println("Smallest number is: " + smallest);
+
+        sc.close();
+    }
+}
