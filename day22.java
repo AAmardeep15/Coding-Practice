e953f4598adb668945c323a490dc3e95644d3c4c
amardeep <mauryaamardeep5264@gmail.com>
2026-05-05 22:04:03 +0530

    day 22

--- day22.java
+++ day22.java
@@ -22,8 +22,6 @@ public class day5 {
         System.out.print(node.data + " ");
         printInorder(node.right);
     }
-
-    // Preorder traversal: Root, Left, Right
     void printPreorder(Node node) {
         if (node == null)
             return;
