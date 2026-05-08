cf2755b49d9a50163bdab52dd810d9a72a3620d3
amardeep <mauryaamardeep5264@gmail.com>
2026-05-08 22:39:53 +0530

    day 25

---
+++ day25.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class day10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+
+        if (input == null || input.trim().isEmpty()) {
+            System.out.println("Number of words: 0");
+        } else {
+            String[] words = input.trim().split("\\s+");
+            System.out.println("Number of words (strings) in the input: " + words.length);
+        }
+        
+        scanner.close();
+    }
+}
