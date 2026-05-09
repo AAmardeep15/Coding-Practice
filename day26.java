4a99915cca98df5963a79b8742459a401621ce0d
amardeep <mauryaamardeep5264@gmail.com>
2026-05-09 18:31:45 +0530

    day 26

--- day26.java
+++ day26.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class day10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a String: ");
         String input = scanner.nextLine();
         
 
