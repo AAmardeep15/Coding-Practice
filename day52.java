1a1ba7c2bfe111ff3623ca46e8d7f935d558d778
amardeep <mauryaamardeep5264@gmail.com>
2026-05-29 23:44:33 +0530

    day 52

---
+++ day52.java
@@ -0,0 +1,67 @@
+public class day48 {
+
+}
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+public class day5 {
+    Node root;
+
+    public day5() {
+        root = null;
+    }
+
+    void printInorder(Node node) {
+        if (node == null)
+            return;
+
+        printInorder(node.left);
+        System.out.print(node.data + " ");
+        printInorder(node.right);
+    }
+
+    void printPreorder(Node node) {
+        if (node == null)
+            return;
+
+        System.out.print(node.data + " ");
+        printPreorder(node.left);
+        printPreorder(node.right);
+    }
+
+    void printPostorder(Node node) {
+        if (node == null)
+            return;
+
+        printPostorder(node.left);
+        printPostorder(node.right);
+        System.out.print(node.data + " ");
+    }
+
+    public static void main(String[] args) {
+        day5 tree = new day5();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.println("Preorder traversal (Root, Left, Right):");
+        tree.printPreorder(tree.root);
+
+        System.out.println("\n\nInorder traversal (Left, Root, Right):");
+        tree.printInorder(tree.root);
+
+        System.out.println("\n\nPostorder traversal (Left, Right, Root):");
+        tree.printPostorder(tree.root);
+        System.out.println();
+    }
+}
